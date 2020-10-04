63debe8cb9f5d6105f4950a1f32a047f36ba0ac4
StanojevicNikola <stanojevic.business@gmail.com>
2020-10-04 23:00:43 +0200

    code cleanup

---
+++ src/test_preparation.py
@@ -0,0 +1,21 @@
+# load the prepared dataset
+from numpy import load
+from matplotlib import pyplot
+# load the dataset
+data = load('../data/maps_256.npz')
+src_images, tar_images = data['arr_0'], data['arr_1']
+print('Loaded: ', src_images.shape, tar_images.shape)
+if(src_images.shape == tar_images.shape):
+    print('Test passed!')
+# plot source images
+n_samples = 3
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + i)
+	pyplot.axis('off')
+	pyplot.imshow(src_images[i].astype('uint8'))
+# plot target image
+for i in range(n_samples):
+	pyplot.subplot(2, n_samples, 1 + n_samples + i)
+	pyplot.axis('off')
+	pyplot.imshow(tar_images[i].astype('uint8'))
+pyplot.show()
